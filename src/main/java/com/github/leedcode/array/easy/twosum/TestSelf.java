e6ad02d12855398067e90cfb9a844fae4e475178
LiuAiKe <lyflycq@163.com>
2019-05-30 16:46:12 +0800

    Add two sum

--- src/main/java/com/github/leedcode/array/easy/twosum/TestSelf.java
+++ src/main/java/com/github/leedcode/array/easy/twosum/TestSelf.java
@@ -3,34 +3,36 @@ package com.github.leedcode.array.easy.twosum;
 import com.alibaba.fastjson.JSON;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author yifeng.liu
  * @date 2019-05-30 09:16
  */
+
+/**
+ * Runtime: 2 ms, faster than 99.27% of Java online submissions for Two Sum.
+ * Memory Usage: 36.4 MB, less than 99.39% of Java online submissions for Two Sum.
+ * Time complexity : O(n^2)
+ * Space complexity : O(1)
+ */
 public class TestSelf {
     public static void main(String[] args) {
-        twoSum(new int[]{1,2,3,4},3);
-        System.out.println(JSON.toJSONString(twoSum(new int[]{1,2,3,4},3)));
+        twoSum(new int[]{1,2,7},9);
+        System.out.println(JSON.toJSONString(twoSum(new int[]{1,2,7},9)));
     }
 
     public static int[] twoSum(int[] nums, int target) {
-
-        int[] result = new int[2];
-        if (nums == null || nums.length < 2) {
-            return result;
-        }
-        HashMap<Integer, Integer> map = new HashMap();
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
-            int tmp = target - nums[i];
-            if (map.containsKey(tmp)) {
-                result[0] = map.get(tmp);
-                result[1] = i;
-                return result;
-            } else {
-                map.put(nums[i], i);
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
             }
         }
-        return result;
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
--- src/main/java/com/github/leedcode/array/easy/twosum/TwoSum.java
+++ src/main/java/com/github/leedcode/array/easy/twosum/TwoSum.java
@@ -6,10 +6,19 @@ import com.alibaba.fastjson.JSON;
  * @author yifeng.liu
  * @date 2019-05-29 18:27
  */
-public class TwoSun {
+
+/**
+ * Runtime: 14 ms, faster than 44.24% of Java online submissions for Two Sum.
+ * Memory Usage: 36.9 MB, less than 99.29% of Java online submissions for Two Sum.
+ *
+ * 复杂性分析
+ * 时间复杂度： O（n ^ 2）。对于每个元素，我们尝试通过循环遍历其余的数组来找到它的补码O（n）时间。因此，时间复杂度是O（n ^ 2）
+ * 空间复杂度： O（1）。
+ */
+public class TwoSum {
     public static void main(String[] args) {
-        twoSum(new int[]{1,2,3,4},3);
-        System.out.println(JSON.toJSONString(twoSum(new int[]{1,2,3,4},3)));
+        twoSum(new int[]{1,2,7},9);
+        System.out.println(JSON.toJSONString(twoSum(new int[]{1,2,7},9)));
     }
 
     public static int[] twoSum(int[] nums, int target) {
